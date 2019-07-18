07a5d12d5b4adcc36277f514ab458b41f9960295
Anjuka <anjukako@gmail.com>
2019-07-18 17:32:19 +0530

    Splash Screen Done

---
+++ app/src/main/java/com/anjukakoralage/hondapromo/SelectLanguageActivity.java
@@ -0,0 +1,14 @@
+package com.anjukakoralage.hondapromo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectLanguageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_language);
+    }
+}
